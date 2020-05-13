b0583eed457630641b9272d30e3ce5c06c5df172
henok tilahun <henokcity@yahoo.com>
2020-05-13 18:31:48 -0400

    adding images and refining the code

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 var passport = require("passport");
 var LocalStrategy = require("passport-local").Strategy;
 
@@ -21,9 +22,8 @@ passport.use(new LocalStrategy(
         return done(null, false, {
           message: "Incorrect email."
         });
-      }
-      // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
+        // If there is a user with the given email, but the password the user gives us is incorrect
+      } else if (!dbUser.validPassword(password)) {
         return done(null, false, {
           message: "Incorrect password."
         });
--- public/js/members.js
+++ public/js/members.js
@@ -1,12 +1,15 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-use-before-define */
 $(document).ready(function() {
-  // Getting references to our form and input
+  // Getting references to our form, input click events
   var signUpForm = $("form.review");
   var category = $("#category");
   var kindOfCoffee = $("input#kindOfCoffee");
   var ratingInput= $("#rating-input");
   var mainReview = $("textarea#main-review");
+  var allRating = $("#allRating");
+  var addNew = $("#addNew");
+  var reviewsContainer = $(".card-group");
 
   $(signUpForm).on("submit", function (event) {
     event.preventDefault();
@@ -37,46 +40,50 @@ $(document).ready(function() {
     $.post("/api/members", Post);
   }
 
+  //Getting users data form db
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.firstName);
   });
 
-  var reviewsContainer = $(".card-group");
+  //Add new button click event to display only the review form
+  $(addNew).on("click", function(){
+    window.location.href = "/members";
+  });
+
+  //Initiating get review
+  $(allRating).on("click", getReviews);
+
 
   // Our initial review array
   var reviews = [];
 
-  // Getting reviews from database when page loads
-  // $(document).on("click", "#allRating", getReviews);
-  getReviews();
 
-  // This function resets the todos displayed with new todos from the database
+  // This function resets the reviews to displayed new reviews from the database
   function initializeRows() {
-    // $todoContainer.empty();
     var rowsToAdd = [];
     for (var i = 0; i < reviews.length; i++) {
       rowsToAdd.push(createNewRow(reviews[i]));
     }
-    reviewsContainer.prepend(rowsToAdd);
   }
-  // This function grabs todos from the database and updates the view
+  // This function grabs reviews from the database and updates the view
   function getReviews() {
     $.get("/api/reviews", function(reviewsData) {
       reviews = reviewsData;
+      $(".review").css("display", "none");
       initializeRows();
     });
   }
 
-  // This function constructs a todo-item row
+  // This function constructs a review item row
   function createNewRow(review) {
     var newInputRow = $(
       [
-        "<div class='card mb-3' style='max-width: 540px;'>",
-        "<div class='row row-cols-1 row-cols-md-3 no-gutters'>",
+        "<div class='card mb-3' style='max-width: 540px;float: right;'>",
+        "<div class='row no-gutters'>",
         "<div class='col-md-4'>",
-        "<img src='/public/assets/img/coffee-img.jpg' class='card-img' alt='beautiful-coffee-img'>",
+        "<img id='coffeeImg' src='{{ asset('public/assets/img/frappuccino.JPG') }}'>",
         "</div>",
-        "<div class='col-md-4'>",
+        "<div class='col-md-8'>",
         "<div class='card-body'>",
         "<h4 class='card-title'>",
         review.coffeeCategory,
@@ -84,10 +91,14 @@ $(document).ready(function() {
         "<h5>",
         review.coffeeName,
         "</h5>",
+        "<p class='card-text'>Rating: <span>",
+        review.rating,
+        " </span>",
+        "</p>",
         "<p class='card-text'>",
         review.coffeeReview,
         "</p>",
-        "<p class='card-text'><small class='text-muted'>Last updated by:<span>",
+        "<p class='card-text'><small class='text-muted'>Last updated by: <span>",
         review.User.firstName,
         "<br>",
         review.updatedAt,
@@ -98,6 +109,10 @@ $(document).ready(function() {
         "</div>"
       ].join("")
     );
+    if (review.coffeeCategory === "Iced Coffee") {
+      $("#coffeeImg").css("src","public/assets/img/iced-coffee.jpg");
+
+    }
     reviewsContainer.append(newInputRow);
   }
 });
