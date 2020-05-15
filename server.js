1297c7b2b978f540ccf45b8bbb90b4008ddee49b
Jasmine Epps <epps.jasmine@gmail.com>
2020-05-14 21:37:14 -0400

    I have added an additional drinks api route, a reviews.js in the public folder to enable dropdown functionalisty of coffee names, added a views folder, completed main handlebars for members page.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* eslint-disable linebreak-style */
 // Requiring necessary npm packages
 var express = require("express");
+var exphbs = require("express-handlebars");
 var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
@@ -14,6 +15,8 @@ var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
@@ -22,6 +25,7 @@ app.use(passport.session());
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+require("./routes/review-api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 // db.sequelize.sync({ force: true }).then(function() {
