136b4119d03e10ccb19d580109db0f410cc50572
henok tilahun <henokcity@yahoo.com>
2020-05-17 17:05:57 -0400

    modified html routes for members

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -18,17 +18,17 @@ module.exports = function(app) {
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   app.get("/signup", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
+    // if (req.user) {
+    //   res.redirect("/members");
+    // }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
